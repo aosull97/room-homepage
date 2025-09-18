0020803fab9275202dc4665e12ec36a4f70290df
Alice <aosullivan@netpremacy.com>
2025-09-18 14:25:50 +0100

    Shop Now link component created

---
+++ src/components/ShopNow.jsx
@@ -0,0 +1,17 @@
+const ShopNow = () => {
+  return (
+    <div>
+        <div className='flex border-2 items-center gap-4'>
+            <div className='pt-1'>
+                <p className='tracking-[0.5rem]'>SHOP NOW</p>
+            </div>
+            <div>
+                <img src='/images/icon-arrow.svg' alt='arrow icon' className='' />
+            </div>
+        
+        </div>
+    </div>
+  )
+}
+
+export default ShopNow
