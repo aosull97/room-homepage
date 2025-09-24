10d77a1cff4b68e88f8b8b1231308e0055951878
Alice <aosullivan@netpremacy.com>
2025-09-24 14:58:32 +0100

    Responsive design applied to slider

--- src/components/Slide1.jsx
+++ src/components/Slide1.jsx
@@ -3,8 +3,8 @@ import SliderButton from "./SliderButton";
 
 const Slide1 = ({ slideRight, slideLeft }) => {
   return (
-    <div className="">
-      <div className="md:hidden relative">
+    <div className="lg:flex">
+      <div className="lg:hidden relative">
         <img
           src="/images/mobile-image-hero-1.jpg"
           alt="Image of white chairs seated by a wooden table with a bonsai tree ontop"
@@ -14,19 +14,19 @@ const Slide1 = ({ slideRight, slideLeft }) => {
           <SliderButton slideRight={slideRight} slideLeft={slideLeft} />
         </div>
       </div>
-      <div className="w-2/3 contain-content hidden">
+      <div className="w-3/5 contain-content hidden lg:block">
         <img
           src="/images/desktop-image-hero-1.jpg"
           alt="Image of white chairs seated by a wooden table with a bonsai tree ontop"
           className="w-full"
         />
       </div>
-      <div>
-        <div className="px-6 pt-10">
+      <div className="lg:w-2/5 lg:place-self-center relative h-100%">
+        <div className="px-6 pt-10 md:px-14 ">
           <p className="font-semibold text-4xl">
             Discover innovative ways to decorate
           </p>
-          <p className="text-grey-500 py-4">
+          <p className="text-grey-500 py-4 md:text-xl lg:text-sm">
             We provide unmatched quality, comfort, and style for property owners
             across the country. Our experts combine form and function in
             bringing your vision to life. Create a room in your own style with
@@ -34,9 +34,12 @@ const Slide1 = ({ slideRight, slideLeft }) => {
             you love.
           </p>
           <div className="pt-4 pb-16">
-          <ShopNow />
+            <ShopNow />
           </div>
         </div>
+      </div>
+      <div className="absolute bottom-0 left-3/5 hidden lg:block">
+        <SliderButton slideRight={slideRight} slideLeft={slideLeft} />
       </div>
     </div>
   );
--- src/components/Slide2.jsx
+++ src/components/Slide2.jsx
@@ -1,47 +1,47 @@
 import ShopNow from "./ShopNow";
 import SliderButton from "./SliderButton";
 
-const Slide2 = ({slideRight, slideLeft}) => {
+const Slide2 = ({ slideRight, slideLeft }) => {
   return (
-    <div className="">
-      <div className="md:hidden relative">
+    <div className="lg:flex">
+      <div className="lg:hidden relative">
         <img
           src="/images/mobile-image-hero-2.jpg"
-          alt="Image of white chairs seated by a wooden table with a bonsai tree ontop"
+          alt="Image of three chairs, one yellow, one grey and one pink"
           className="w-full"
         />
         <div className="absolute bottom-0 right-0">
           <SliderButton slideRight={slideRight} slideLeft={slideLeft} />
         </div>
       </div>
-      <div className="w-2/3 contain-content hidden">
+      <div className="w-3/5 contain-content hidden lg:block">
         <img
           src="/images/desktop-image-hero-2.jpg"
-          alt="Image of white chairs seated by a wooden table with a bonsai tree ontop"
+          alt="Image of three chairs, one yellow, one grey and one pink"
           className="w-full"
         />
       </div>
-      <div>
-        <div className="px-6 pt-10">
+      <div className="lg:w-2/5 lg:place-self-center relative h-100%">
+        <div className="px-6 pt-10 md:px-14 ">
           <p className="font-semibold text-4xl">
             We are available all across the globe
           </p>
-          <p className="text-grey-500 py-4">
+          <p className="text-grey-500 py-4 md:text-xl lg:text-sm">
             With stores all over the world, it's easy for you to find furniture
             for your home or place of business. Locally, we’re in most major
             cities throughout the country. Find the branch nearest you using our
             store locator. Any questions? Don't hesitate to contact us today.
           </p>
           <div className="pt-4 pb-16">
-          <ShopNow />
+            <ShopNow />
           </div>
         </div>
+      </div>
+      <div className="absolute bottom-0 left-3/5 hidden lg:block">
+        <SliderButton slideRight={slideRight} slideLeft={slideLeft} />
       </div>
     </div>
   );
 };
 
 export default Slide2;
-
-
-
