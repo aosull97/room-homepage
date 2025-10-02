9dc07b7625678dd110fe6e918115a227fad8f9a7
Alice <aosullivan@netpremacy.com>
2025-10-02 11:44:18 +0100

    Styling fix

--- src/components/DesktopNav.jsx
+++ src/components/DesktopNav.jsx
@@ -2,12 +2,12 @@ import logo from "/images/logo.svg";
 
 const DesktopNav = () => {
   return (
-    <div className="flex p-16 lg:p-10 gap-12 text-white items-center">
+    <div className="flex p-16 lg:p-14 gap-12 text-white items-center">
         <div className="">
-          <img src={logo} alt="logo" width={82} />
+          <img src={logo} alt="logo" width={64} />
         </div>
         <div className="">
-          <ul className="flex gap-8 text-xl md:gap-14 md:text-2xl lg:text-lg">
+          <ul className="flex gap-8 text-xl md:text-2xl lg:text-lg">
             <li className="hover:cursor-pointer">h<span className="hover:underline hover:underline-offset-12">om</span>e</li>
             <li className="hover:cursor-pointer">s<span className="hover:underline hover:underline-offset-12">ho</span>p</li>
             <li className="hover:cursor-pointer">a<span className="hover:underline hover:underline-offset-12">bou</span>t</li>
